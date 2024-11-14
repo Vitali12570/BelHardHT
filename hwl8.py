935ce515b81783efdd14f980de2fdb96ff71f5cf
KorzikVitali <korzikvitali@gmail.com>
2024-11-14 20:21:55 +0100

    123

--- hwl8.py
+++ hwl8.py
@@ -2,6 +2,7 @@ from abc import ABC,abstractmethod
 
 
 class Transport(ABC):
+
     @abstractmethod
     def __init__(self, brand: str, model: str, issue_year: int, color: str, milage = 0):
         self.brand = brand
@@ -30,12 +31,36 @@ class Car(Transport):
         self.milage = milage
 
     def move(self, num_km: float):
-        if num_km <= 0:
-            raise ValueError('Расстояние должно быть положительным числом')
+        try:
+            num_km <= 0
+        except ValueError:
+            print('Расстояние должно быть положительтным числом')
         else:
             self.milage += num_km
+        print(f'{self.brand}{self.model}({self.color}-{self.issue_year}) проехала {self.milage} км')
 
-a = Car('Volvo', 'xc40', 2020, 'Bronze', 'diesel')
-a.move(1)
-print(a.milage)
+class Airplane(Transport):
 
+    def __init__(self, brand: str, model: str, issue_year: int,color: str, lifting_capacity: int, milage = 0):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.issue_year = issue_year
+        self.lifting_capacity = lifting_capacity
+        self.milage = milage
+
+
+    def move(self, num_km: float):
+        try:
+            num_km <= 0
+        except ValueError:
+            print('Расстояние должно быть положительтным числом')
+        else:
+            self.milage += num_km
+        print(f'{self.brand} {self.model} ({self.color}-{self.issue_year})(грузоподъемность: {self.lifting_capacity} кг) пролетел {self.milage} км')
+
+volvo1 = Car('Volvo', 'xc40', 2020, 'Bronze', 'diesel')
+volvo1.move(1234)
+
+jet1 =Airplane('Jet', 'qwe', 2024, 'White', 30000)
+jet1.move(1000000)
---
+++ lesson9.py
@@ -0,0 +1,3 @@
+import re
+
+print(re.search(r'\d', 'qwe123'))
